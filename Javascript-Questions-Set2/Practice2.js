aa1021970065f8c187bd353d5588ba2f714a1bc0
Shivani Oswal <shivanimoswal@gmail.com>
2024-04-04 17:48:30 +0530

    Javascript Practice Set

---
+++ Javascript-Questions-Set2/Practice2.js
@@ -0,0 +1,25 @@
+/*  1.) At a restaurant you ate: 1 Dal 100 rupee, 2 Roti's 10 each, 1 Ice-cream 30 rupee, calculate and display the final bill amount.
+    2.) Calculate 18% GST on iPhone15 79990 rupees and 2 airpods pro 24990 rupee each.
+    3.) Create String using single, back and double quotes
+    4.) Concatenate String with Strings and String with numbers.
+    5.) Create order summary String for our Myntra cart
+    6.) Display order summary in popup.
+*/
+
+//1.) 100+(2*10)+30
+//    o/p->  150
+
+
+//2.) (79990+79990*(18/100))+(2*24990)
+//    o/p->   144368.2
+
+
+//3.) 'Hello World' --> 'Hello World'
+//    "Hello World" --> 'Hello World'
+//    `Hello World` --> 'Hello World'
+
+
+//4.) "Hello" + "Shivani" --> 'HelloShivani'
+//    "Hello"+"12345"  --> 'Hello12345'
+
+//5.) see in the video 
