b6ce5bdb9a1591fb6b3fe66fac5b3b0b1c86b698
Shivani Oswal <shivanimoswal@gmail.com>
2024-04-07 14:51:40 +0530

    Practice code

---
+++ Javascript-Questions-Set2/Practice1.js
@@ -0,0 +1,12 @@
+/* 1.) Use an alert to display good Morning
+   2.) Display your name in a pop-up
+   3.) Using Math calculate the following:- 75-25, 3+3-5
+   4.) Change Facebook page to display I am learning JS */
+
+   //1.) alert("Good Morning");
+
+   //2.) alert("Shivani");
+
+   //3.) Directly type the operation in console
+
+   document.body.innerHTML="I am learning Javascript";
